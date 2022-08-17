bdb3e2c06a3dac21d1862cdb3e579a65fa381cd4
Raza <owaisraza1704@gmail.com>
2022-08-17 22:52:05 +0530

    access argument0 elements

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function(insert,remove) {
-
+const removeFromArray = function() {
+let array ="";
+for(i=0;i<arguments[0].length;i++){
+    console.log(arguments[0][i]);
+}
 };
 // Do not edit below this line
 module.exports = removeFromArray;
