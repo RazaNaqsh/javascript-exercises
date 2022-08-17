2fe009aea8014b81c1c03683b25651eec927b9af
Raza <owaisraza1704@gmail.com>
2022-08-17 12:24:45 +0530

    Complete lesson 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+let reversed = '';
+    return reversed = word.split("").reverse().join("");
 };
 
 // Do not edit below this line
