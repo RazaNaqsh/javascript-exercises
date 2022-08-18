00d28528072e447aaca59130e8f142a7e96d9b3e
Raza <owaisraza1704@gmail.com>
2022-08-18 08:09:03 +0530

    Did sumall on my own with few helps from odin

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
-
+//pseudocode first
+// start limit given and end limit given so
+// start a loop from i=start till end and just add that to sum
+// so if the larger number is given first ,then create a check condition
+// that if a > b then i=a and b is limit and i++
+// and if b>a then i=b and a is limit and i--
+const sumAll = function(num1,num2) {
+let sum=0 ;
+let a =num1;
+let b=num2;
+console.log(a,b);
+if(a<0 || b<0 || typeof(b)!=="number"){
+    return "ERROR";
+}
+else if(a < b){
+    for(let i=a;i<=b;i++){
+        sum+=i;
+    }
+}
+else if( a>b){
+    for(let i=b;i<=a;i++){
+        sum+=i;
+    }
+}
+return sum;
 };
 
 // Do not edit below this line
