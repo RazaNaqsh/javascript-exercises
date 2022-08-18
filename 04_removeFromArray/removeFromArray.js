b9b406da8c32e99064f106f310a39ba2fba86138
Raza <owaisraza1704@gmail.com>
2022-08-18 06:21:14 +0530

    Another failed attempt

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -3,7 +3,26 @@
 //do nothing else add that element into new array and return that
 
 const removeFromArray = function() {
-
+let array=[...arguments[0]];
+console.log(array);
+let newArray=[];
+let removedValue=[];
+for(i=1;i<arguments.length;i++){
+    for(j=0;j<array.length;j++)
+    {
+        if(array[j].includes(arguments[i]))
+        {
+            removedValue.push(array[j]);
+        }
+        else
+        {
+            newArray.push(array[j]);
+        }
+        console.log("inserted value :"+ newArray);
+        console.log("deleted value :"+ removedValue);
+    }
+    return newArray;
+}
 };
 // Do not edit below this line
 module.exports = removeFromArray;
