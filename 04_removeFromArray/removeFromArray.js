5e6c21a7d483fd3bfbd33c54fec80fae0d6dc276
Raza <owaisraza1704@gmail.com>
2022-08-18 05:40:15 +0530

    New logic to try

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,27 +1,9 @@
-//lets write beautiful pseudocode in here
-// 1- accessing what arguments have
-// we see the first thing is always insert
-//rest are to delete ones
-// start a for loop that access whole loop
-// when at argument[0], inside it run again a loop from arg[1]
-//run a loop of to remove ele, and inside it a loop of arg[0] so that compare
-//all of arg[o] elements with to del
-// compare it with rest of arg elements 
-// when it equals with the elements of arg[0], then do nothing
-// when its not enter that into new Array!
-
+//store the first insert into a var which needs to be inserted
+//run a loop from arg[1], and check if it includes in tobe inserted var, then 
+//do nothing else add that element into new array and return that
 
 const removeFromArray = function() {
-let array =[];
 
-for(i=1;i<arguments.length;i++){
-    for(j=0;j<arguments[0].length;j++){
-        if(arguments[i] !== arguments[0][j]){
-            array.push(arguments[0][j]);
-        }
-    }
-}
-return array;
 };
 // Do not edit below this line
 module.exports = removeFromArray;
