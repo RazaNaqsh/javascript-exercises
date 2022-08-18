78413be3dbea8b99d0f0c887559f4c75c0eec9b7
Raza <owaisraza1704@gmail.com>
2022-08-18 09:07:26 +0530

    Temp one complete easy

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,18 @@
-const ftoc = function() {
+//pseudocode
+// first lets tackle ftoc
+// we take f as input and put into c formula and then round it to one dec place return it
+// same with ctof, input , put formula, round off
 
+const ftoc = function(temp) {
+ let celsius = (temp-32)*5/9;
+ let tempCelcius = Math.round(celsius*10)/10;
+ return tempCelcius;
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+let fahrenheit= (temp*9/5)+32;
+let tempFahren = Math.round(fahrenheit*10)/10;
+return tempFahren;
 };
 
 // Do not edit below this line
